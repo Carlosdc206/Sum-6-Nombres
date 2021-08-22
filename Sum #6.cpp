5755d13234e6b0a7ffd61c7abae0ae0c38e2b86b
Carlosdc206 <89317743+Carlosdc206@users.noreply.github.com>
2021-08-21 22:08:17 -0500

    Add files via upload

---
+++ Sum #6.cpp
@@ -0,0 +1,37 @@
+
+using namespace std;
+main(){
+	string Ana[10];
+	int i=0
+	int c=0, d=0, pos=0;
+	for(i=0;i<10;i++){
+	 cout<<"ingrese los nombres "<<Jose;
+	 cin>>ana[i];
+	}
+	for(i=0;i<10;i++){
+	 cout <<"nombres de la lista "<<Ana[i]<<" ";
+	}
+	string mascorto=Ana[0];
+	 int mayor=mascorto.length();
+	for(i=0;i<10;i++){
+	 string comparacion=arr[i];
+	 if(c<mayor){
+	 	mayor=c;
+	 	pos=i;
+	}
+   }
+   cout<<"nombre mas corto "<<Ana[pos]<<Jose;
+   
+   string maslargo=arr[0];
+   int menor= maslargo.length();
+   for(i=0;i<10;i++){
+   	  string comparacion=arr[i];
+   	  if(d>menor){
+		  menor=d;
+		  pos=i;
+	  }
+	}
+	cout<<"nombre mas largo "<<Ana[pos]<<Jose;
+	
+	return 0;
+}
